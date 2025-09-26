89d55699000c3b65164a3c9193b9872e24ae9c61
dharmendra kariya <22043860+dharmendrakariya@users.noreply.github.com>
2025-09-26 15:22:00 +0530

    Update update_prod_if_release.sh

--- release-prod/update_prod_if_release.sh
+++ release-prod/update_prod_if_release.sh
@@ -42,9 +42,14 @@ mkdir -p "$(dirname "$FILE_PATH")"
 echo "kustomize:" > "$FILE_PATH"
 echo "  images:" >> "$FILE_PATH"
 
+log "CUSTOM_IMAGE_LIST: $CUSTOM_IMAGE_LIST"
+log "FILE_PATH: $FILE_PATH"
+log "TAG: $TAG"
+
 if [[ -n "${CUSTOM_IMAGE_LIST:-}" ]]; then
   log "ℹ️ Using custom image list:"
-  echo "$CUSTOM_IMAGE_LIST" | tr '|' '\n' | while read -r image; do
+  # echo "$CUSTOM_IMAGE_LIST" | tr '|' '\n' | while read -r image; do
+  echo "$CUSTOM_IMAGE_LIST" | while read -r image; do
     trimmed=$(echo "$image" | xargs)
     [[ -n "$trimmed" ]] && echo "    - ${trimmed}:${TAG}" >> "$FILE_PATH"
   done
